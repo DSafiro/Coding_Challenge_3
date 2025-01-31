4b3387124b7c67abf0be730ee14ef28ce7db67bc
Dylan <safirodylan@gmail.com>
2025-01-31 00:51:23 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,5 @@
+// Task 1: Product Price Management
+let price = [120, 350, 657, 751, 965] // Initial array of prices
+price.push(75) // Adds new price to array
+price.splice(0, 1) // Removes first price in array
+console.log("Updated Prices:", price) //  Outputs updated prices into console
